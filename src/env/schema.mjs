31abb2a9bea536a6d6f1d157a359c9160935c24b
kulgg <75735874+kulgg@users.noreply.github.com>
2022-10-16 14:09:51 +0200

    Add copy invite link

--- src/env/schema.mjs
+++ src/env/schema.mjs
@@ -20,6 +20,7 @@ export const serverSchema = z.object({
 export const clientSchema = z.object({
   // NEXT_PUBLIC_CLIENTVAR: z.string(),
   NEXT_PUBLIC_PUSHER_KEY: z.string(),
+  NEXT_PUBLIC_HOST: z.string(),
 });
 
 /**
@@ -31,4 +32,5 @@ export const clientSchema = z.object({
 export const clientEnv = {
   // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
   NEXT_PUBLIC_PUSHER_KEY: process.env.NEXT_PUBLIC_PUSHER_KEY,
+  NEXT_PUBLIC_HOST: process.env.NEXT_PUBLIC_HOST,
 };
